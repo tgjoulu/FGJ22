da77e39af6492660a70a712962c04a5786603880
Pete Sevander <pete.sevander@gmail.com>
2022-01-29 15:50:03 +0200

    Initial Waaave

--- src/scenes/stage.ts
+++ src/scenes/stage.ts
@@ -1,5 +1,6 @@
 import Constants from '../constants';
 import Player from '../player/player';
+import Wave from '../objects/wave';
 
 enum WorldSide {
     Light,
@@ -20,6 +21,8 @@ export default class StageScene extends Phaser.Scene {
     private drums: Phaser.Sound.BaseSound;
     private bass: Phaser.Sound.BaseSound;
 
+    private wave: Phaser.GameObjects.Sprite;
+
     constructor() {
         super({ key: 'StageScene' });
     }
@@ -34,6 +37,7 @@ export default class StageScene extends Phaser.Scene {
         });
         this.load.audio('drums', 'assets/sound/drums.wav');
         this.load.audio('bass', 'assets/sound/bass.wav');
+        this.load.image('waveSprite', 'assets/sprites/wave.png');
     }
 
     create() {
@@ -59,6 +63,9 @@ export default class StageScene extends Phaser.Scene {
             Constants.WINDOW_WIDTH,
             Constants.WINDOW_HEIGHT
         );
+
+        var mapBounds = this.aboveLight.getBounds();
+        this.wave = new Wave(this, mapBounds.right, mapBounds.top, mapBounds.height);
     }
 
     _debugRenderTileCollisions(tileMap: Phaser.Tilemaps.Tilemap) {
@@ -130,5 +137,7 @@ export default class StageScene extends Phaser.Scene {
         });
     }
 
-    update() {}
+    update(time:number, dt: number) {
+      this.wave.update(time, dt);
+    }
 }
