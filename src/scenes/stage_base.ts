79a73e392f836ab9efc21a4a1d0ff16ca5c53c3b
Pete Sevander <pete.sevander@gmail.com>
2022-01-30 14:16:43 +0200

    volumesäätöä

--- src/scenes/stage_base.ts
+++ src/scenes/stage_base.ts
@@ -42,6 +42,7 @@ export default class StageSceneBase extends Phaser.Scene {
     private bgAnalogMusicLoops: Phaser.Sound.BaseSound[];
     private bgDigitalMusicLoops: Phaser.Sound.BaseSound[];
 
+    private collectableVol: integer;
     private analDrumVol: integer;
     private analBassVol: integer;
     private digiDrumVol: integer;
@@ -104,9 +105,11 @@ export default class StageSceneBase extends Phaser.Scene {
         this.wolfSound = this.sound.add('wolf');
 
 
-        this.analDrumVol = 0.05;
+        this.collectableVol = 0.1;
+
+        this.analDrumVol = 0.1;
         this.analBassVol = 1;
-        this.digiDrumVol = 0.05;
+        this.digiDrumVol = 0.1;
         this.digiBassVol = 0.2;
 
         this.bgAnalogMusicLoops = [bgAnalDrums, bgAnalBass, bgAnalPads, bgAnalLead];
@@ -409,7 +412,7 @@ export default class StageSceneBase extends Phaser.Scene {
 
     _checkPlayerBounds() {
         if (this.player.y > this.physics.world.bounds.bottom) {
-            this.deathSound.play( {volume: 0.5});
+            this.deathSound.play( {volume: 0.4});
             this._restartScene();
         }
     }
@@ -439,7 +442,7 @@ export default class StageSceneBase extends Phaser.Scene {
     };
 
     _onCollectableCollide = () => {
-        this.crystalSound.play( {volume: 0.5});
+        this.crystalSound.play( {volume: this.collectableVol});
         this.collectableCount--;
         console.log(this.collectableCount);
     };
